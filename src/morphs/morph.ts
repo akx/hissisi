4aa6b95b3656cc4d803a32a1b6cce3a3898ffae1
Aarni Koskela <akx@iki.fi>
2024-09-14 21:37:10 +0300

    wip

--- src/morphs/morph.ts
+++ src/morphs/morph.ts
@@ -3,29 +3,63 @@ import { scroll } from "./scroll.ts";
 import { getNormPhase, getPixelPhaseWidth } from "./phase.ts";
 import { getMorpher, register } from "./registry.ts";
 import { MorphOptions } from "./types.ts";
+import { applyEasingWithOptions } from "../easing.ts";
 
-register("Flicker", (d1, d2, [w, h], phase, { direction }) =>
-  pixelwise(d1, d2, (c1, c2, x, y) =>
-    Math.random() * getNormPhase(x, y, w, h, direction, 1) > phase ? c1 : c2,
-  ),
+register(
+  "Flicker",
+  (d1, d2, [w, h], phase, { direction, easing1, easing2 }) =>
+    pixelwise(d1, d2, (c1, c2, x, y) =>
+      applyEasingWithOptions(easing2, Math.random()) *
+        applyEasingWithOptions(
+          easing1,
+          getNormPhase(x, y, w, h, direction, 1),
+        ) >
+      phase
+        ? c1
+        : c2,
+    ),
+  {
+    supportsEasings: 2,
+    easing1Name: "Movement Mapping",
+    easing2Name: "Probability Mapping",
+  },
 );
 
-register("Cover", (d1, d2, [w, h], phase, { direction }) =>
-  pixelwise(d1, d2, (c1, c2, x, y) =>
-    getNormPhase(x, y, w, h, direction) < phase ? c2 : c1,
-  ),
+register(
+  "Cover",
+  (d1, d2, [w, h], phase, { direction, easing1 }) =>
+    pixelwise(d1, d2, (c1, c2, x, y) =>
+      applyEasingWithOptions(easing1, getNormPhase(x, y, w, h, direction)) <
+      phase
+        ? c2
+        : c1,
+    ),
+  {
+    supportsEasings: 1,
+    easing1Name: "Edge Mapping",
+  },
 );
 
-register("CoverWithBorder", (d1, d2, [w, h], phase, { direction }) => {
-  const ppw = getPixelPhaseWidth(direction, w, h) / 2;
-  return pixelwise(d1, d2, (c1, c2, x, y) => {
-    const nph = getNormPhase(x, y, w, h, direction);
-    if (Math.abs(nph - phase) < ppw) {
-      return false;
-    }
-    return nph < phase ? c2 : c1;
-  });
-});
+register(
+  "CoverWithBorder",
+  (d1, d2, [w, h], phase, { direction, easing1 }) => {
+    const ppw = getPixelPhaseWidth(direction, w, h) / 2;
+    return pixelwise(d1, d2, (c1, c2, x, y) => {
+      const nph = applyEasingWithOptions(
+        easing1,
+        getNormPhase(x, y, w, h, direction),
+      );
+      if (Math.abs(nph - phase) < ppw) {
+        return false;
+      }
+      return nph < phase ? c2 : c1;
+    });
+  },
+  {
+    supportsEasings: 1,
+    easing1Name: "Edge Mapping",
+  },
+);
 
 register(
   "Scroll",
